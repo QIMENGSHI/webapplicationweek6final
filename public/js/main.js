b9fcdeb031ecc53e8507509d431eb31042865db5
QIMENGSHI <qms030313@outlook.com>
2024-12-09 13:18:41 +0200

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,13 @@ const form = document.getElementById("offerForm");
 form.addEventListener('submit', async (e) => {
     e.preventDefault(); // Prevent default submission
 
-    // const formData = new FormData(form);
+    const formData = new FormData(form);
 
-    const formData = {
-        title: document.getElementById('title').value,
-        price: document.getElementById('price').value,
-        description: document.getElementById('description').value,
-      };
+    // const formData = {
+    //     title: document.getElementById('title').value,
+    //     price: document.getElementById('price').value,
+    //     description: document.getElementById('description').value,
+    //   };
     
       try {
         const response = await fetch('/upload', {
